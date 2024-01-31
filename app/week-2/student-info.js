4aaee08dbd8b11e9c3d1f32585959f016d902755
Matt6362 <matt6362@gmail.com>
2024-01-30 20:54:45 -0700

    Update StudentInfo
    Fixed the link, I literally just forgot to import from next/link

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function StudentInfo() {
 
     return (
         <nav>
             <h1>Matthew Hansen</h1>
-            <h1>Github account</h1>
+            <Link href="https://github.com/MrMatt6362/cprg306-assignments">Github</Link>
         </nav>
     );
 }
