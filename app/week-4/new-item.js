42fe51d258109ec5e8e88d54a82f3ed7916c7890
Matt6362 <matt6362@gmail.com>
2024-02-13 22:34:54 -0700

    handleSubmit modifications
    Still messing with handleChange

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -9,12 +9,17 @@ export default function ShoppingForm() {
 
     const handleSubmit = (event) => {
       event.preventDefault();
-      alert("Added item: " + name + ", quantity: " + quantity + ", category: " + category);
+      let item = {name, quantity, category};
+      console.log(item);
+      alert("Added item: " + item.name + ", quantity: " + item.quantity + ", category: " + item.category);
+      setName("");
+      setQuantity(1);
+      setCategory("produce");
     }
 
-    const handleChange = () => {
-
-    }
+    const handleChange = (event) => {
+        setName(event.target.value.toUpperCase());
+    };
 
     return (
         <form onSubmit={handleSubmit}>
