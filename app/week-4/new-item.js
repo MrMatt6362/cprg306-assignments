e05d89ccfb28ed9b1ed3f50b529993f19a427a45
Matt6362 <matt6362@gmail.com>
2024-02-13 22:26:34 -0700

    Fixed lots of broken things
    With the power of common sense (I know this is bad documentation but I need to fill my coffee)

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -1,6 +1,28 @@
 "use client"
 import { useState } from "react";
 
-const handleSubmit = () => {
-    event.preventDefault();
+export default function ShoppingForm() {
+
+    const [name, setName] = useState("");
+    const [quantity, setQuantity] = useState(1);
+    const [category, setCategory] = useState("produce");
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      alert("Added item: " + name + ", quantity: " + quantity + ", category: " + category);
+    }
+
+    const handleChange = () => {
+
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Item:
+                <input type="text" placeholder="Enter item name" value={name} onChange={handleChange}/>
+            </label>
+            <input type="submit" value="[ + ]" />
+        </form>
+    );
 }
--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,10 +1,10 @@
+import ShoppingForm from "./new-item";
 import StudentInfo from "./new-item";
-import { useState } from "react";
 
 export default function Page() {
     return (
       <main>
-        
+        <ShoppingForm />
       </main>
     );
   }
